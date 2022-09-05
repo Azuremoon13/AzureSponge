5879c88a5fe17b35e7a106b364f9fb16dfb93971
Azuremoon13 <joe.bolt@icloud.com>
2022-09-05 20:53:13 +0100

    V1.0.1

--- src/main/kotlin/xyz/azuremoon/ASEventListener.kt
+++ src/main/kotlin/xyz/azuremoon/ASEventListener.kt
@@ -3,6 +3,7 @@ package xyz.azuremoon
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.block.Block
+import org.bukkit.block.data.Waterlogged
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockPlaceEvent
@@ -12,19 +13,32 @@ class ASEventListener : Listener {
 
     @EventHandler
     fun onSpongePlace(e: BlockPlaceEvent){
-        if (e.blockPlaced.type != Material.SPONGE || !e.player.hasPermission("sponge.use")) return
+        if (e.blockPlaced.type != Material.SPONGE || !e.player.hasPermission("sponge.use") || e.block.isLiquid) return
         sphereAround(e.blockPlaced.location, ConfigController.spongeRadius).forEach {
             when (it.type) {
+                Material.KELP_PLANT -> {it.breakNaturally(); it.type = Material.AIR}
+                Material.KELP -> {it.breakNaturally(); it.type = Material.AIR}
+                Material.SEAGRASS -> it.type = Material.AIR
+                Material.TALL_SEAGRASS -> it.type = Material.AIR
                 Material.WATER -> it.type = Material.AIR
                 Material.LAVA -> if (e.player.hasPermission("sponge.lava")) {
                     it.type = Material.AIR
                 }
                 else -> {}
             }
+            if (it.blockData is Waterlogged && ConfigController.clearWaterlogged) {
+                val wl: Waterlogged = it.blockData as Waterlogged
+                if (wl.isWaterlogged) {
+                    wl.isWaterlogged = false
+                    it.blockData = wl
+                    it.state.update()
+                }
+            }
         }
         if (e.player.hasPermission("sponge.dry")) {
             e.blockPlaced.type = Material.SPONGE
         }
+        else {e.blockPlaced.type = Material.WET_SPONGE}
     }
 
     private fun sphereAround(location: Location, radius: Int): List<Block> {
--- src/main/kotlin/xyz/azuremoon/util/ConfigController.kt
+++ src/main/kotlin/xyz/azuremoon/util/ConfigController.kt
@@ -5,8 +5,10 @@ import xyz.azuremoon.AzureSponge
 object ConfigController {
 
     private const val SPONGERADIUS = "spongeRadius"
+    private const val CLEARWATERLOG = "clearWaterLog"
 
     private const val DEFAULT_SPONGERADIUS = 20
+    private const val DEFAULT_CLEARWATERLOG = true
 
     val spongeRadius: Int
         get() {
@@ -15,5 +17,13 @@ object ConfigController {
                 ?.getInt(SPONGERADIUS, DEFAULT_SPONGERADIUS)
                 ?.takeIf { it >= 0 }
                 ?: DEFAULT_SPONGERADIUS
+        }
+
+    val clearWaterlogged: Boolean
+        get() {
+            return AzureSponge.instance
+                ?.config
+                ?.getBoolean(CLEARWATERLOG, DEFAULT_CLEARWATERLOG)
+                ?: DEFAULT_CLEARWATERLOG
         }
 }
